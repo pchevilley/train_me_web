836dcb7b765497d4336481c5d4c09e5a8e696589
Paul Chevilley <paul.chevilley@gmail.com>
2021-09-26 00:35:13 -0400

    app bar fine tuning

--- src/user_space/AppBar.tsx
+++ src/user_space/AppBar.tsx
@@ -9,6 +9,7 @@ export function AppBar() {
     return (
         <section className={'app-bar ' + (menuVisible && "app-bar--extended")}>
             <AppBarButton icon="bars" onClick={() => setMenuVisible(!menuVisible)}/>
+            <AppBarButton icon="calendar-week" label="Clubs" onClick={() => {}} />
             <AppBarButton icon="calendar-week" label="Calendar" onClick={() => {}} />
             <AppBarButton icon="users" label="Members" onClick={() => {}} />
             <AppBarButton icon="bell" label="Notifications" onClick={() => {}} />
